29e56b4a07d19dbb5a1147217dd5460d5a85dcaf
Mariusz Najwer <najwer23@gmail.com>
2023-12-29 15:05:07 +0100

    Sorting mdx

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,15 +1,23 @@
 import type { Preview } from "@storybook/react";
 
 const preview: Preview = {
-  parameters: {
-    actions: { argTypesRegex: "^on[A-Z].*" },
-    controls: {
-      matchers: {
-        color: /(background|color)$/i,
-        date: /Date$/i,
-      },
-    },
-  },
+	parameters: {
+		actions: { argTypesRegex: "^on[A-Z].*" },
+		controls: {
+			matchers: {
+				color: /(background|color)$/i,
+				date: /Date$/i,
+			},
+		},
+		options: {
+			storySort: {
+				order: [
+					"Getting Started",
+					"Colors"
+				]
+			},
+		},
+	},
 };
 
 export default preview;
