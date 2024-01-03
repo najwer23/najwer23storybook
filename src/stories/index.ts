7c477797f986d8fa3f074a7116aaa7697956689a
Mariusz Najwer <najwer23@gmail.com>
2024-01-03 09:00:38 +0100

    One File

---
+++ src/stories/index.ts
@@ -0,0 +1,5 @@
+export { Button } from "./button";
+export { MenuAside } from "./menuAside";
+export { Carousel } from "./carousel";
+export { Input } from "./input";
+export { Textarea } from "./textarea";
