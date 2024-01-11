0024700241562e5f9d342828291604c8acdaef06
Mariusz Najwer <najwer23@gmail.com>
2024-01-11 18:46:00 +0100

    Footer

---
+++ src/stories/footer/Footer.stories.tsx
@@ -0,0 +1,24 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { Footer } from './Footer';
+
+const meta: Meta<typeof Footer> = {
+	title: "Footer/Footer",
+	component: Footer,
+	parameters: {
+		layout: "centered",
+	},
+	tags: ["autodocs"],
+	argTypes: {
+	},
+};
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const FooterAlwaysOnBottom: Story = {
+	args: {
+
+	},
+};
+
---
+++ src/stories/footer/Footer.tsx
@@ -0,0 +1,19 @@
+import styles from "./index.module.css"
+import { Text } from "../text"
+
+export const Footer = () => {
+	return (
+		<>
+			<footer>
+				<div className={styles["footerPush"]}></div>
+				<div className={styles["footerContainer"]}>
+					<div className={styles["footerContent"]}>
+						<Text kind="p">
+							© Mariusz Najwer
+						</Text>
+					</div>
+				</div>
+			</footer>
+		</>
+	)
+};
