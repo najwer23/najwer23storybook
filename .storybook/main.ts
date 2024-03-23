ddc25f2a066b1ca14a4e7f06309258c8b34c8e43
Mariusz Najwer <najwer23@gmail.com>
2024-03-23 15:56:21 +0100

    Stroybook 8 + Vite

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,35 +1,19 @@
-import type { StorybookConfig } from "@storybook/react-webpack5";
+import type { StorybookConfig } from "@storybook/react-vite";
 
 const config: StorybookConfig = {
-	stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
-	addons: [
-		"@storybook/addon-links",
-		"@storybook/addon-essentials",
-		"@storybook/preset-create-react-app",
-		"@storybook/addon-onboarding",
-		"@storybook/addon-interactions",
-		"storybook-css-modules-preset",
-	],
-	framework: {
-		name: "@storybook/react-webpack5",
-		options: {
-			builder: {
-				useSWC: true,
-			},
-		},
-	},
-	swc: () => ({
-		jsc: {
-			transform: {
-				react: {
-					runtime: "automatic",
-				},
-			},
-		},
-	}),
-	docs: {
-		autodocs: "tag",
-	},
-	staticDirs: ["..\\public"],
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@chromatic-com/storybook",
+    "@storybook/addon-interactions",
+  ],
+  framework: {
+    name: "@storybook/react-vite",
+    options: {},
+  },
+  docs: {
+    autodocs: "tag",
+  },
 };
 export default config;
--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,34 +1,15 @@
 import type { Preview } from "@storybook/react";
-import "./Theme.css"
+import "./Theme.css";
 
 const preview: Preview = {
-	parameters: {
-		actions: { argTypesRegex: "^on[A-Z].*" },
-		controls: {
-			matchers: {
-				color: /(background|color)$/i,
-				date: /Date$/i,
-			},
-		},
-		options: {
-			storySort: {
-				order: ["Getting Started", "CSS variables"],
-			},
-		},
-		backgrounds: {
-			default: "--n23-color-surface",
-			values: [
-				{
-					name: "--n23-color-surface",
-					value: "var(--n23-color-surface)",
-				},
-				{
-					name: "white",
-					value: "#fff",
-				},
-			],
-		},
-	},
+  parameters: {
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+  },
 };
 
 export default preview;
