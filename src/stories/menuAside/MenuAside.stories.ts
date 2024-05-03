869acad9513f771513ab0d1e33a12a497c8a1dd2
Mariusz Najwer <najwer23@gmail.com>
2024-05-03 15:21:03 +0200

    menuAside

--- src/stories/menuAside/MenuAside.stories.ts
+++ src/stories/menuAside/MenuAside.stories.ts
@@ -1,6 +1,7 @@
-import type { Meta, StoryObj } from '@storybook/react';
+import type { Meta, StoryObj } from "@storybook/react";
 
 import { MenuAside } from "./MenuAside";
+import { MenuAsideType } from "./index.types";
 
 const meta: Meta<typeof MenuAside> = {
 	title: "Menu/MenuAside",
@@ -12,85 +13,145 @@ const meta: Meta<typeof MenuAside> = {
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-export const MenuLeftAside: Story = {
-	args: {
-		menuTitle: "N",
-		menuItems: [
-			[
-				{
-					menuTitle: "Menu",
-					items: [
-						{
-							itemLink: "link1",
-							itemLinkText: "link1 link1 link1 link1 link1 link1 likn",
-							itemLinkType: "hash",
-						},
-						{
-							itemLink: "link2",
-							itemLinkText: "link2",
-							itemLinkType: "hash",
-						},
-					],
-				},
-				{
-					menuTitle: "Projects",
-					items: [
-						{
-							itemLink: "link1",
-							itemLinkText: "link1",
-							itemLinkType: "hash",
-						},
-						{
-							itemLink: "link2",
-							itemLinkText: "link2",
-							itemLinkType: "hash",
-						},
-					],
-				},
-			],
+const MENU_ASIDE: MenuAsideType = {
+	menuTitle: "N",
+	menuItems: [
+		[
+			{
+				menuTitle: "Menu",
+				items: [
+					{
+						itemLink: "",
+						itemLinkText: "Home",
+						itemLinkType: "hash",
+					},
+					{
+						itemLink: "gallery",
+						itemLinkText: "Gallery",
+						itemLinkType: "hash",
+					},
+					{
+						itemLink: "quotes",
+						itemLinkText: "Quotes",
+						itemLinkType: "hash",
+					},
+					{
+						itemLink: "weather",
+						itemLinkText: "Weather",
+						itemLinkType: "hash",
+					},
+					{
+						itemLink: "apod",
+						itemLinkText: "NASA - APOD",
+						itemLinkType: "hash",
+					},
+					{
+						itemLink: "contact",
+						itemLinkText: "Contact",
+						itemLinkType: "hash",
+					},
+					{
+						itemLink: "404",
+						itemLinkText: "404",
+						itemLinkType: "hash",
+					},
+				],
+			},
+			{
+				menuTitle: "Projects",
+				items: [
+					{
+						itemLink: "https://leetcode.com/najwer23/",
+						itemLinkText: "Leetcode",
+						itemLinkType: "linkOut",
+					},
+					{
+						itemLink: "https://najwer23.github.io/react-redux-ts/",
+						itemLinkText: "Geometric figures",
+						itemLinkType: "linkOut",
+					},
+					{
+						itemLink: "https://najwer23.github.io/sudoku/",
+						itemLinkText: "Sudoku",
+						itemLinkType: "linkOut",
+					},
+					{
+						itemLink: "https://jabber.cyclic.app/",
+						itemLinkText: "Jabber",
+						itemLinkType: "linkOut",
+					},
+					{
+						itemLink:
+							"https://najwer23.github.io/najwer23storybook/?path=/docs/getting-started--docs",
+						itemLinkText: "Design system",
+						itemLinkType: "linkOut",
+					},
+					{
+						itemLink: "https://restilo.pl/",
+						itemLinkText: "Restilo",
+						itemLinkType: "linkOut",
+					},
+					{
+						itemLink: "https://tueuropa.pl/ubezpieczenia-mieszkania-i-domu.htm",
+						itemLinkText: "My house",
+						itemLinkType: "linkOut",
+					},
+					{
+						itemLink:
+							"https://tueuropa.pl/ubezpieczenia-sportowe/ubezpieczenie-nnw-sport.htm",
+						itemLinkText: "Active in Sport",
+						itemLinkType: "linkOut",
+					},
+					{
+						itemLink:
+							"https://tueuropa.pl/ubezpieczenia-zycia-i-zdrowia/ubezpieczenie-twojego-zdrowia-myhealth.htm",
+						itemLinkText: "My Health",
+						itemLinkType: "linkOut",
+					},
+					{
+						itemLink: "https://tueuropa.pl/ubezpieczenia-turystyczne.htm",
+						itemLinkText: "Itravel",
+						itemLinkType: "linkOut",
+					},
+					{
+						itemLink: "https://tvn.pl/programy/top-model",
+						itemLinkText: "TVN. Top Model",
+						itemLinkType: "linkOut",
+					},
+					{
+						itemLink: "https://tvn.pl/program-tv",
+						itemLinkText: "TVN. Electronic program guide",
+						itemLinkType: "linkOut",
+					},
+					{
+						itemLink: "https://tvn24.pl/",
+						itemLinkText: "TVN24",
+						itemLinkType: "linkOut",
+					},
+					{
+						itemLink: "https://tvn24.pl/biznes/notowania/waluty",
+						itemLinkText: "TVN24. Stock quotes",
+						itemLinkType: "linkOut",
+					},
+					{
+						itemLink: "https://tvn24.pl/wybory-samorzadowe-2024",
+						itemLinkText: "TVN24. Polish Local Government Elections 2024",
+						itemLinkType: "linkOut",
+					},
+				],
+			},
 		],
-	},
+	],
 };
 
+export const MenuLeftAside: Story = {
+	args: {
+		...MENU_ASIDE
+	},
+};
 
 export const MenuLeftAsideWithoutLogo: Story = {
 	args: {
-		menuItems: [
-			[
-				{
-					menuTitle: "Menu",
-					items: [
-						{
-							itemLink: "link1",
-							itemLinkText: "link1 link1 link1 link1 link1 link1 likn",
-							itemLinkType: "hash",
-						},
-						{
-							itemLink: "link2",
-							itemLinkText: "link2",
-							itemLinkType: "hash",
-						},
-					],
-				},
-				{
-					menuTitle: "Projects",
-					items: [
-						{
-							itemLink: "link1",
-							itemLinkText: "link1",
-							itemLinkType: "hash",
-						},
-						{
-							itemLink: "link2",
-							itemLinkText: "link2",
-							itemLinkType: "hash",
-						},
-					],
-				},
-			],
-		],
+		...MENU_ASIDE,
 	},
 };
-
-
-
