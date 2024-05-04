6be6a5750fa722bddce146466ad7b6d26d962d90
Mariusz Najwer <najwer23@gmail.com>
2024-05-04 13:44:32 +0200

    Rewrites Text component

--- src/stories/text/Text.tsx
+++ src/stories/text/Text.tsx
@@ -1,3 +1,4 @@
+import { createElement } from "react";
 import styles from "./index.module.css"
 import { TextTypes } from "./index.types";
 
@@ -5,15 +6,7 @@ export const Text = ({
 	kind,
 	children
 }: TextTypes) => {
-
-	const textDictionary = {
-		"p": <p>{children}</p>,
-		"pSmall": <p>{children}</p>,
-		"pSmallBold": <p>{children}</p>,
-		"h1": <h1>{children}</h1>,
-		"h2": <h2>{children}</h2>,
-		"h3": <h3>{children}</h3>,
-		"default": children
-	}
-	return <div className={styles[`${kind}Text`]}>{textDictionary[kind ?? "default"]}</div>
+	return <>
+		{createElement(kind.includes("h") ? kind : "p", { className: styles[kind + "Text"] }, children)}
+	</>
 };
--- src/stories/text/index.types.ts
+++ src/stories/text/index.types.ts
@@ -1,6 +1,6 @@
 import { ReactNode } from "react";
 
 export interface TextTypes {
-	kind?: "p" | "h1" | "h2" | "h3" | "pSmall" | "pSmallBold",
+	kind: "p" | "h1" | "h2" | "h3" | "pSmall" | "pSmallBold",
 	children: ReactNode
 }
