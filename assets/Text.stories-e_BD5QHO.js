2fbb10f8a47cb3854dc80fa72d43170118cd9b94
Mariusz Najwer <najwer23@gmail.com>
2024-05-04 13:59:54 +0200

    Updates

---
+++ assets/Text.stories-e_BD5QHO.js
@@ -0,0 +1,36 @@
+import{T as _}from"./Text-07thdpZ1.js";import"./jsx-runtime-QvZ8i92b.js";import"./index-uubelm5h.js";const j={title:"Text/Text",component:_,parameters:{layout:"centered"},tags:["autodocs"],argTypes:{}},c="It was Sam's first view of a battle of Men against Men, and he did not like it much. He was glad that he could not see the dead face. He wondered what the man's name was and where he came from; and if he was really evil of heart, or what lies or threats had led him on the long march from his home; and if he would rather have stayed there in peace.",e={args:{kind:"h1",children:"Title! More title!"}},r={args:{kind:"h2",children:"Title! More title! h2"}},a={args:{kind:"h3",children:"Title! More title! h3"}},t={args:{kind:"p",children:c}},s={args:{kind:"pSmall",children:c}},n={args:{kind:"pSmallBold",children:c}},o={args:{kind:"pSubTitle",children:c}};var i,d,l;e.parameters={...e.parameters,docs:{...(i=e.parameters)==null?void 0:i.docs,source:{originalSource:`{
+  args: {
+    kind: "h1",
+    children: "Title! More title!"
+  }
+}`,...(l=(d=e.parameters)==null?void 0:d.docs)==null?void 0:l.source}}};var h,m,p;r.parameters={...r.parameters,docs:{...(h=r.parameters)==null?void 0:h.docs,source:{originalSource:`{
+  args: {
+    kind: "h2",
+    children: "Title! More title! h2"
+  }
+}`,...(p=(m=r.parameters)==null?void 0:m.docs)==null?void 0:p.source}}};var g,T,u;a.parameters={...a.parameters,docs:{...(g=a.parameters)==null?void 0:g.docs,source:{originalSource:`{
+  args: {
+    kind: "h3",
+    children: "Title! More title! h3"
+  }
+}`,...(u=(T=a.parameters)==null?void 0:T.docs)==null?void 0:u.source}}};var x,S,k;t.parameters={...t.parameters,docs:{...(x=t.parameters)==null?void 0:x.docs,source:{originalSource:`{
+  args: {
+    kind: "p",
+    children: testText
+  }
+}`,...(k=(S=t.parameters)==null?void 0:S.docs)==null?void 0:k.source}}};var f,w,H;s.parameters={...s.parameters,docs:{...(f=s.parameters)==null?void 0:f.docs,source:{originalSource:`{
+  args: {
+    kind: "pSmall",
+    children: testText
+  }
+}`,...(H=(w=s.parameters)==null?void 0:w.docs)==null?void 0:H.source}}};var M,P,b;n.parameters={...n.parameters,docs:{...(M=n.parameters)==null?void 0:M.docs,source:{originalSource:`{
+  args: {
+    kind: "pSmallBold",
+    children: testText
+  }
+}`,...(b=(P=n.parameters)==null?void 0:P.docs)==null?void 0:b.source}}};var y,B,v;o.parameters={...o.parameters,docs:{...(y=o.parameters)==null?void 0:y.docs,source:{originalSource:`{
+  args: {
+    kind: "pSubTitle",
+    children: testText
+  }
+}`,...(v=(B=o.parameters)==null?void 0:B.docs)==null?void 0:v.source}}};const q=["TextH1","TextH2","TextH3","TextParagraph","TextParagraphSmall","TextParagraphSmallBold","TextParagraphSubTitle"];export{e as TextH1,r as TextH2,a as TextH3,t as TextParagraph,s as TextParagraphSmall,n as TextParagraphSmallBold,o as TextParagraphSubTitle,q as __namedExportsOrder,j as default};
