32f3eaf9e2a53cd7d4a602fdfcd1bc28533e1351
Mariusz Najwer <najwer23@gmail.com>
2024-05-04 13:54:28 +0200

    pSubTitleText

--- src/stories/text/index.types.ts
+++ src/stories/text/index.types.ts
@@ -1,6 +1,6 @@
 import { ReactNode } from "react";
 
 export interface TextTypes {
-	kind: "p" | "h1" | "h2" | "h3" | "pSmall" | "pSmallBold",
-	children: ReactNode
+	kind: "p" | "h1" | "h2" | "h3" | "pSmall" | "pSmallBold" | "pSubTitle";
+	children: ReactNode;
 }
