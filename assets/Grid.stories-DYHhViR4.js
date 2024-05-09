08c9b5238ba2e818e22607d41bd9dec05e6e6880
Mariusz Najwer <najwer23@gmail.com>
2024-05-09 22:22:54 +0200

    Updates

---
+++ assets/Grid.stories-DYHhViR4.js
@@ -0,0 +1,67 @@
+import{j as e}from"./jsx-runtime-QvZ8i92b.js";import{r as f}from"./index-uubelm5h.js";const w="_gridContainer_19br1_7",v="_gridContainerChild_19br1_21",o={gridContainer:w,gridContainerChild:v},k=({children:t,gap:n,col:r})=>e.jsx(e.Fragment,{children:e.jsx("div",{className:o.gridContainer,style:{"--n23-fc-m":r.mobile,"--n23-fc-t":r.tablet,"--n23-fc-sd":r.smallDesktop,"--n23-fc-d":r.desktop,"--n23-fg-c":n.col,"--n23-fg-r":n.row},children:f.Children.map(t.props.children,b=>e.jsx("div",{className:o.gridContainerChild,children:b}))})});k.__docgenInfo={description:"",methods:[],displayName:"Grid",props:{children:{required:!0,tsType:{name:"JSX.Element"},description:""},gap:{required:!0,tsType:{name:"signature",type:"object",raw:`{\r
+	col: string,\r
+	row: string\r
+}`,signature:{properties:[{key:"col",value:{name:"string",required:!0}},{key:"row",value:{name:"string",required:!0}}]}},description:""},col:{required:!0,tsType:{name:"signature",type:"object",raw:`{\r
+	mobile: number;\r
+	smallDesktop: number\r
+	desktop: number;\r
+	tablet: number\r
+}`,signature:{properties:[{key:"mobile",value:{name:"number",required:!0}},{key:"smallDesktop",value:{name:"number",required:!0}},{key:"desktop",value:{name:"number",required:!0}},{key:"tablet",value:{name:"number",required:!0}}]}},description:""}}};const L={title:"Grid/Grid",component:k,parameters:{layout:"centered"},tags:["autodocs"]},s={args:{gap:{col:"20px",row:"20px"},col:{smallDesktop:3,desktop:3,mobile:3,tablet:3},children:e.jsx(e.Fragment,{children:Array.from({length:15},(t,n)=>e.jsx("div",{children:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."},n))})}},i={args:{gap:{col:"20px",row:"20px"},col:{smallDesktop:4,desktop:5,mobile:1,tablet:3},children:e.jsx(e.Fragment,{children:Array.from({length:15},(t,n)=>e.jsx("div",{children:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."},n))})}},a={args:{gap:{col:"20px",row:"20px"},col:{smallDesktop:4,desktop:5,mobile:1,tablet:3},children:e.jsx(e.Fragment,{children:Array.from({length:30},(t,n)=>e.jsxs("div",{children:[" ",e.jsx("img",{style:{width:"100%",aspectRatio:1,height:"auto"},src:"https://source.unsplash.com/random/500x500",height:"500",width:"500",alt:""})," "]},n))})}};var l,d,m;s.parameters={...s.parameters,docs:{...(l=s.parameters)==null?void 0:l.docs,source:{originalSource:`{
+  args: {
+    gap: {
+      col: "20px",
+      row: "20px"
+    },
+    col: {
+      smallDesktop: 3,
+      desktop: 3,
+      mobile: 3,
+      tablet: 3
+    },
+    children: <>\r
+                {Array.from({
+        length: 15
+      }, (_, i) => <div key={i}>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</div>)}\r
+            </>
+  }
+}`,...(m=(d=s.parameters)==null?void 0:d.docs)==null?void 0:m.source}}};var p,u,c;i.parameters={...i.parameters,docs:{...(p=i.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  args: {
+    gap: {
+      col: "20px",
+      row: "20px"
+    },
+    col: {
+      smallDesktop: 4,
+      desktop: 5,
+      mobile: 1,
+      tablet: 3
+    },
+    children: <>\r
+                {Array.from({
+        length: 15
+      }, (_, i) => <div key={i}>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</div>)}\r
+            </>
+  }
+}`,...(c=(u=i.parameters)==null?void 0:u.docs)==null?void 0:c.source}}};var h,g,y;a.parameters={...a.parameters,docs:{...(h=a.parameters)==null?void 0:h.docs,source:{originalSource:`{
+  args: {
+    gap: {
+      col: "20px",
+      row: "20px"
+    },
+    col: {
+      smallDesktop: 4,
+      desktop: 5,
+      mobile: 1,
+      tablet: 3
+    },
+    children: <>\r
+                {Array.from({
+        length: 30
+      }, (_, i) => <div key={i}> <img style={{
+          width: "100%",
+          aspectRatio: 1,
+          height: "auto"
+        }} src="https://source.unsplash.com/random/500x500" height="500" width="500" alt="" /> </div>)}\r
+            </>
+  }
+}`,...(y=(g=a.parameters)==null?void 0:g.docs)==null?void 0:y.source}}};const C=["Default","GridDynamicColumnsBasedOnWidth","GridDynamicColumnsWithImgsBasedOnWidth"];export{s as Default,i as GridDynamicColumnsBasedOnWidth,a as GridDynamicColumnsWithImgsBasedOnWidth,C as __namedExportsOrder,L as default};
