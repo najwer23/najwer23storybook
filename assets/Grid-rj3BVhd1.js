ab3dbbef36871756cb71dead04efc5ded5024b3a
Mariusz Najwer <najwer23@gmail.com>
2024-05-14 22:14:54 +0200

    Updates

--- assets/Grid-rj3BVhd1.js
+++
@@ -1,9 +0,0 @@
-import{j as r}from"./jsx-runtime-QvZ8i92b.js";import{r as a}from"./index-uubelm5h.js";const o="_gridContainer_19br1_7",d="_gridContainerChild_19br1_21",i={gridContainer:o,gridContainerChild:d},u=({children:s,gap:n,col:e})=>r.jsx(r.Fragment,{children:r.jsx("div",{className:i.gridContainer,style:{"--n23-fc-m":e.mobile,"--n23-fc-t":e.tablet,"--n23-fc-sd":e.smallDesktop,"--n23-fc-d":e.desktop,"--n23-fg-c":n.col,"--n23-fg-r":n.row},children:a.Children.map(s.props.children,t=>t&&r.jsx("div",{className:i.gridContainerChild,children:t}))})});u.__docgenInfo={description:"",methods:[],displayName:"Grid",props:{children:{required:!0,tsType:{name:"any"},description:""},gap:{required:!0,tsType:{name:"signature",type:"object",raw:`{\r
-	col: string,\r
-	row: string\r
-}`,signature:{properties:[{key:"col",value:{name:"string",required:!0}},{key:"row",value:{name:"string",required:!0}}]}},description:""},col:{required:!0,tsType:{name:"signature",type:"object",raw:`{\r
-	mobile: number;\r
-	smallDesktop: number\r
-	desktop: number;\r
-	tablet: number\r
-}`,signature:{properties:[{key:"mobile",value:{name:"number",required:!0}},{key:"smallDesktop",value:{name:"number",required:!0}},{key:"desktop",value:{name:"number",required:!0}},{key:"tablet",value:{name:"number",required:!0}}]}},description:""}}};export{u as G};
