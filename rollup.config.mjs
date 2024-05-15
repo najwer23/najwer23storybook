91645e108a3df5880eddfd5feda4c6441c76e7e9
Mariusz Najwer <najwer23@gmail.com>
2024-05-15 09:00:24 +0200

    TS2

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,6 +1,6 @@
 import peerDepsExternal from "rollup-plugin-peer-deps-external";
 import resolve from "@rollup/plugin-node-resolve";
-import typescript from "@rollup/plugin-typescript";
+import typescript from "rollup-plugin-typescript2";
 import postcss from "rollup-plugin-postcss";
 import terser from "@rollup/plugin-terser";
 
